5b663f11946c1d2c94176f057deb8356f5a7f7ed
Artyu <arthurpejoiane@hotmail.fr>
2023-02-22 13:53:32 +0100

    [ADD] Camera Movement Finish

--- Assets/Script/Camera/CameraMovement.cs
+++ Assets/Script/Camera/CameraMovement.cs
@@ -1,28 +1,50 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public Transform camera;
-    public Transform player;
+    [SerializeField] private Transform player;
     [SerializeField] private float offset = 5f;
     private DirectionState[] allDir = new DirectionState[4] {DirectionState.NORTH, DirectionState.EAST, DirectionState.SOUTH, DirectionState.WEST};
     private int idDir = 0;
+
+    private CinemachineVirtualCamera brain;
+    private CinemachineTransposer body;
+
+    private Transform obstruction;
+
+    private void Awake()
+    {
+        brain = GetComponent<CinemachineVirtualCamera>();
+        body = brain.GetCinemachineComponent<CinemachineTransposer>();
+        brain.Follow = player;
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Debug.Log("Left");
-            camera.DOMove(GetNextPosition(1), 2);
+            //Debug.Log("Left");
+            brain.Follow = null;
+            transform.DOMove(GetNextPosition(1), 2).OnComplete(() =>
+            {
+                ViewObstructed();
+                brain.Follow = player;
+            });
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            Debug.Log("Droite");
-            camera.DOMove(GetNextPosition(-1), 2);
+            //Debug.Log("Droite");
+            brain.Follow = null;
+            transform.DOMove(GetNextPosition(-1), 2).OnComplete(() =>
+            {
+                ViewObstructed();
+                brain.Follow = player;
+            });
         }
     }
 
@@ -43,15 +65,43 @@ public class CameraMovement : MonoBehaviour
         switch (GetNextDirection(modifier))
         {
             case DirectionState.NORTH:
-                return new Vector3(player.position.x, player.position.y, camera.position.z - offset);
+                body.m_FollowOffset = new Vector3(0, 0, -offset);
+                return new Vector3(player.position.x, player.position.y, transform.position.z - offset);
+            
             case DirectionState.EAST:
-                return new Vector3(camera.position.x - offset, player.position.y, player.position.z);
+                body.m_FollowOffset = new Vector3(-offset, 0 , 0);
+                return new Vector3(transform.position.x - offset, player.position.y, player.position.z);
+            
             case DirectionState.SOUTH:
-                return new Vector3(player.position.x, player.position.y, camera.position.z + offset);
+                body.m_FollowOffset = new Vector3(0, 0, offset);
+                return new Vector3(player.position.x, player.position.y, transform.position.z + offset);
+            
             case DirectionState.WEST:
-                return new Vector3(camera.position.x + offset, player.position.y, player.position.z);
+                body.m_FollowOffset = new Vector3(offset, 0, 0);
+                return new Vector3(transform.position.x + offset, player.position.y, player.position.z);
         }
 
         return Vector3.zero;
+    }
+
+    private void ViewObstructed()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, player.position - transform.position, out hit, offset + 4))
+        {
+            if (hit.collider.gameObject.tag != "Player")
+            {
+                obstruction = hit.transform;
+                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+            }
+            else
+            {
+                if (obstruction != null)
+                {
+                    obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                }
+            }
+        }
     }
 }
