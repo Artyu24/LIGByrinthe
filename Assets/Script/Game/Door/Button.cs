43190282305b0f1c635079aa4297520d31a9c079
Artyu <arthurpejoiane@hotmail.fr>
2023-03-26 15:24:50 +0200

    [ADD] Button Animation when pressed

--- Assets/Script/Game/Door/Button.cs
+++ Assets/Script/Game/Door/Button.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Button : MonoBehaviour
@@ -8,6 +9,8 @@ public class Button : MonoBehaviour
 
     private GameObject interactionBubblePrefab;
     private GameObject bubbleTemp = null;
+
+    [SerializeField] private Transform buttonPlate;
 
     private void Awake()
     {
@@ -23,7 +26,12 @@ public class Button : MonoBehaviour
 
         bubbleTemp = Instantiate(interactionBubblePrefab, UIManager.instance.CanvasUI);
         bubbleTemp.GetComponent<InteractionBubble>().SetPos(door.transform.position);
-        
-        //Anim Bouton
+
+        buttonPlate.DOMoveY(-0.12f, 0.3f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        buttonPlate.DOMoveY(0.05f, 0.3f);
     }
 }
