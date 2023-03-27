539133322f3b4156ae24dc6c605f0b9142d219f7
Artyu <arthurpejoiane@hotmail.fr>
2023-03-27 15:17:41 +0200

    [ADD] Player Bubble on Map Camera

--- Assets/Script/Player/PlayerSprite.cs
+++ Assets/Script/Player/PlayerSprite.cs
@@ -1,11 +1,26 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerSprite : MonoBehaviour
 {
+    private RectTransform rect;
+
+    private void Start()
+    {
+        GameObject playerBubble = Resources.Load<GameObject>("PlayerBubble");
+        rect = Instantiate(playerBubble, UIManager.instance.CanvasUI).GetComponent<RectTransform>();
+    }
+
     void LateUpdate()
     {
         transform.rotation = Quaternion.Euler(0f, Camera.main.transform.rotation.eulerAngles.y, 0f);
+    }
+
+    public void Update()
+    {
+        rect.position = MapCameraManager.LvlCamera.WorldToScreenPoint(transform.position);
     }
 }
